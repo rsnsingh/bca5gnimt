2db7756b50106647a5a5ff45410f7e3e12026397
Ravinder Singh <42645016+rsnsingh@users.noreply.github.com>
2022-10-19 14:37:20 +0530

    Update table.php

--- table.php
+++ table.php
@@ -1,8 +1,15 @@
-<?php    
-define('a', 7);   
-for($i=1; $i<=10; $i++)   
-{   
-  echo $i*a;   
-  echo '<br>';     
-}  
+<!DOCTYPE html>
+<html>
+<body>
+<?php  
+  <h1>My first PHP page</h1>
+  echo "Hello World!";
+  define('a', 7);   
+  for($i=1; $i<=10; $i++)   
+   {   
+     echo $i*a;   
+     echo '<br>';     
+   }  
 ?> 
+</body>
+</html>
